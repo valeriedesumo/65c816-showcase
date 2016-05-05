7d885d5cee613395ab9e6156991ad89fd5e4ba94
Valerie Desumo <valeriedesumo@users.noreply.github.com>
2016-05-05 12:04:28 -0700

    Update power_exchange.asm

--- blocks/power_exchange.asm
+++ blocks/power_exchange.asm
@@ -7,13 +7,13 @@ Below:
 Above:
 Side:
 LDA $19     ; Powerup status
-BEQ Small  ; $19 is 0 if small Mario, so jump ahead
-INC $0DBE  ; Increase lives
+BEQ Small   ; $19 is 0 if small Mario, so jump ahead
+INC $0DBE   ; Increase lives
 STZ $19     ; Remove powerup
 LDA #$05    ; Life sfx
 BRA Return
 Small:
-LDA #$2A   ; Incorrect sfx
+LDA #$2A    ; Incorrect sfx
 Return:
 STA $1DFC   ; Play sfx
 
