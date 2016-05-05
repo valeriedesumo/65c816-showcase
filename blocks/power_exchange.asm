18c7e11a8d7774637956cdc66a88c8a10fa23b8e
Valerie Desumo <valeriedesumo@users.noreply.github.com>
2016-05-05 12:46:22 -0700

    Update power_exchange.asm

--- blocks/power_exchange.asm
+++ blocks/power_exchange.asm
@@ -6,16 +6,16 @@ JMP Below : JMP Above : JMP Side : JMP X : JMP X : JMP X : JMP X
 Below:
 Above:
 Side:
-LDA $19     ; Powerup status
-BEQ Small   ; $19 is 0 if small Mario, so jump ahead
-INC $0DBE   ; Increase lives
-STZ $19     ; Remove powerup
-LDA #$05    ; Life sfx
+LDA $19    ; Powerup status
+BEQ Small  ; $19 is 0 if small Mario, so jump ahead
+INC $0DBE  ; Increase lives
+STZ $19    ; Remove powerup
+LDA #$05   ; Life sfx
 BRA Return
 Small:
-LDA #$2A    ; Incorrect sfx
+LDA #$2A   ; Incorrect sfx
 Return:
-STA $1DFC   ; Play sfx
+STA $1DFC  ; Play sfx
 
 X:
 RTL
