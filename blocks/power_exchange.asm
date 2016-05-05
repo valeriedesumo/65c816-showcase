fd64d18abefe32cc7522ef04ea56592e7f09963e
Valerie Desumo <valeriedesumo@users.noreply.github.com>
2016-05-05 12:03:29 -0700

    Update power_exchange.asm

--- blocks/power_exchange.asm
+++ blocks/power_exchange.asm
@@ -1,21 +1,21 @@
-;Exchanges Mario's powerup for one life, otherwise plays an "incorrect" sound
-;No credit needed
+; Exchanges Mario's powerup for one life, otherwise plays an "incorrect" sound
+; No credit needed
 
 JMP Below : JMP Above : JMP Side : JMP X : JMP X : JMP X : JMP X
 
 Below:
 Above:
 Side:
-LDA $19    ;powerup status
-BEQ Small  ;$19 is 0 if small Mario, so jump ahead
-INC $0DBE  ;increase lives
-STZ $19    ;remove powerup
-LDA #$05   ;life sfx
+LDA $19     ; Powerup status
+BEQ Small  ; $19 is 0 if small Mario, so jump ahead
+INC $0DBE  ; Increase lives
+STZ $19     ; Remove powerup
+LDA #$05    ; Life sfx
 BRA Return
 Small:
-LDA #$2A   ;incorrect sfx
+LDA #$2A   ; Incorrect sfx
 Return:
-STA $1DFC  ;play sfx
+STA $1DFC   ; Play sfx
 
 X:
 RTL
