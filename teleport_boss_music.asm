3dae8ee2cc71cb8a32cb6ae290b5264205422f27
Valerie Desumo <valeriedesumo@users.noreply.github.com>
2016-05-08 23:13:53 -0700

    Create teleport_boss_music.asm

---
+++ teleport_boss_music.asm
@@ -0,0 +1,43 @@
+;	;	;	;	;	;	;	;	;	;	;	;
+;
+;	Teleports Mario after playing the Passed Boss music (0B)
+;
+;	Insert as a generator
+;	Does not use extra bit
+;	Credit preferred
+;	Done at request
+;
+;	;	;	;	;	;	;	;	;	;	;	;
+
+dcb "INIT"
+dcb "MAIN"
+
+LDA $0699
+CMP #$09   ; Warp after 9 seconds, the length of the Passed Boss music
+BEQ Warp
+
+LDA $0698  ; Prevents music reset
+BNE Continue
+
+LDA #$0B   ; Music
+STA $1DFB  ; Play
+LDA #$01   ; Set flag to
+STA $0698  ; prevent music reset
+
+Continue:
+LDA $13	   ; Frame count
+AND #$3F
+BNE Return ; Continue only once per second
+INC $0699  ; Counts seconds
+RTL
+
+Warp:
+LDA #$06  ;\
+STA $71   ; Enter pipe
+STZ $89   ;/
+STZ $88   ; Instantly warp
+STZ $0698 ; Clear flags
+STZ $0699
+
+Return:
+RTL
